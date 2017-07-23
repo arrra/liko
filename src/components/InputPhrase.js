66f8596fe33fc432c302f00811f3fa8de103a581
Sagar Singh <ssing128@gmail.com>
2017-07-23 15:37:09 -0700

    updateing state upon on change

--- src/components/InputPhrase.js
+++ src/components/InputPhrase.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
 class InputPhrase extends Component {
+  constructor(){
+    super()
+    this.state = ({
+      phrase: ''
+    })
+  }
+
+  handleChange(event) {
+    this.setState({phrase: event.target.value})
+  }
+
   render() {
+    console.log(this.state.phrase)
     return (
       <div>
         <h1>Enter a phrase you want to remember</h1>
-        <input type="text"/>
+        <input type="text" onChange={this.handleChange.bind(this)}/>
       </div>
     )
   }
