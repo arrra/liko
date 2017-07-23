41298a43bcc493e180cc5186a291e9509f7e55a4
Sagar Singh <ssing128@gmail.com>
2017-07-23 16:15:32 -0700

    on click add datta to props

--- src/components/InputPhrase.js
+++ src/components/InputPhrase.js
@@ -12,12 +12,16 @@ class InputPhrase extends Component {
     this.setState({phrase: event.target.value})
   }
 
+  handleClick() {
+    this.props.onDone(this.state.phrase)
+  }
+
   render() {
     return (
       <div>
         <h1>Enter a phrase you want to remember</h1>
         <input type="text" onChange={this.handleChange.bind(this)}/>
-        <button>click here when done</button>
+        <button onClick={this.handleClick.bind(this)}>click here when done</button>
       <h2>Phrase: {this.state.phrase}</h2>
       </div>
     )
