a6e82f86608a9d0ba6f53b153e225d3d8761643f
Sagar Singh <ssing128@gmail.com>
2017-07-23 15:23:36 -0700

    user can enter phrase

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+import InputPhrase from './components/InputPhrase'
+
+class App extends Component {
+  render() {
+    return (
+      <InputPhrase/>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/InputPhrase.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class InputPhrase extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Enter a phrase you want to remember</h1>
+        <input type="text"/>
+      </div>
+    )
+  }
+}
+
+export default InputPhrase;
